7fb8194435ddbd9d08819d57928d5a218896d8c3
Jarrett Meyer <jarrettmeyer@gmail.com>
2013-10-08 14:02:33 -0400

    added unit tests for FormParser

---
+++ sample_app_1/FormParser.js
@@ -0,0 +1,24 @@
+var qs = require("querystring");
+
+var FormParser = function (_request) {
+  var self = this;
+
+  self.request = _request;
+  self.body = "";
+  self.obj = {};
+
+  self.getObject = function (callback) {
+    self.request.on("data", function (chunk) {
+      self.body += chunk;
+    });
+    self.request.on("end", function () {
+      self.obj = qs.parse(self.body);
+      if (callback) {
+        callback(self.obj);
+      }
+    });
+  };
+
+};
+
+module.exports = FormParser
---
+++ sample_app_1/tests/FormParser_Tests.js
@@ -0,0 +1,29 @@
+var assert = require("assert");
+var FormParser = require("../FormParser");
+
+describe("FormParser", function () {
+
+  var fakeRequest = {
+    on: function (key, callback) {
+      switch (key) {
+        case "data":
+          callback("name=John+Doe&age=30&notes=This+is+a+sample+user+of+the+application.+It+is+provided+solely+for+testing+purposes.");
+          break;
+        case "end":
+          callback();
+          break;
+      }
+    }
+  };
+
+  describe("#getObject", function () {
+    it("returns the expected object", function (done) {
+      var formParser = new FormParser(fakeRequest);
+      formParser.getObject(function (o){
+        assert.equal("John Doe", o.name);
+        assert.equal(30, o.age);
+        done();
+      });
+    });
+  });
+});
