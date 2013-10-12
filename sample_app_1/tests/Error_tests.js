54ba524b0c237c2cec1c8645c95cbd93561c7d1f
Jarrett Meyer <jarrettmeyer@gmail.com>
2013-10-12 16:01:47 -0400

    added tests for ArgumentError

--- sample_app_1/tests/Error_tests.js
+++ sample_app_1/tests/Error_tests.js
@@ -3,6 +3,16 @@ var error = require("../error");
 
 describe("error.ArgumentError", function () {
 
+  describe("#argumentName", function () {
+
+    it("saves the argument", function (done) {
+      var ae = new error.ArgumentError("variable");
+      assert.equal("variable", ae.argumentName);
+      done();
+    });
+
+  });
+
   describe("#toString", function () {
 
     it("produces the expected message", function (done) {
